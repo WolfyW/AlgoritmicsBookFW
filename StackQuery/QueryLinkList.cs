157bea43355893e0c133848096f2777265d98a33
WolfyW <wolfyw@live.ru>
2019-07-15 16:49:22 +0300

    Реализовал стек и очередь на основе списков

---
+++ StackQuery/QueryLinkList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmicsBookFW.Interface;
+using AlgoritmicsBookFW.LinkedList;
+
+namespace AlgoritmicsBookFW.StackQuery
+{
+    class QueryLinkList<T> : ISQ<T>
+    {
+        private readonly LinkListEnd<T> _linkList;
+
+        public QueryLinkList()
+        {
+            _linkList = new LinkListEnd<T>();
+        }
+
+        public void Insert(T item)
+        {
+            _linkList.AddLast(item);
+        }
+
+        public T Peek()
+        {
+            return _linkList.PeekFirst();
+        }
+
+        public T Remove()
+        {
+            return _linkList.DeleteFirst();
+        }
+
+        public void Display()
+        {
+            _linkList.Display();
+        }
+
+        public bool IsEmpty()
+        {
+            return _linkList.IsEmpty();
+        }
+
+    }
+}
---
+++ StackQuery/StackLinkList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoritmicsBookFW.Interface;
+using AlgoritmicsBookFW.LinkedList;
+
+namespace AlgoritmicsBookFW.StackQuery
+{
+    class StackLinkList<T> : ISQ<T>
+    {
+        private readonly LinkList<T> _linkList;
+
+        public StackLinkList()
+        {
+            _linkList = new LinkList<T>();
+        }
+
+        public void Insert(T item)
+        {
+            _linkList.AddFirst(item);
+        }
+
+        public T Peek()
+        {
+            return _linkList.Peek();
+        }
+
+        public T Remove()
+        {
+            return _linkList.DeleteFirst();
+        }
+
+        public bool IsEmpty()
+        {
+            return _linkList.IsEmpty();
+        }
+
+        public void Dislplay()
+        {
+            _linkList.Display();
+        }
+    }
+}
