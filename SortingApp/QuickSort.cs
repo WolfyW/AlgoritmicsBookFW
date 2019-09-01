a66670a1da1436cd154f47706c826cee1632bd31
WolfyW <Wolfyw@live.ru>
2019-09-01 22:48:50 +0300

    начало быстрой сортировки

---
+++ SortingApp/QuickSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmicsBookFW.SortingApp
+{
+    public class QuickSort<T> : HArray<T> where T : IComparable<T>
+    {
+        public QuickSort(int maxElemnts) : base(maxElemnts)
+        {
+        }
+
+        public QuickSort(T[] array) : base(array)
+        {
+        }
+
+        public override void Sort()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Partition()
+        {
+
+        }
+    }
+}
